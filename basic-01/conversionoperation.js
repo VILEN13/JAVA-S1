b5d5fb6e941d91f81c74e0ec2f154bd1770ba8b0
ANKUSH <114465873+VILEN13@users.noreply.github.com>
2025-06-23 02:38:00 +0000

    new add

---
+++ basic-01/conversionoperation.js
@@ -0,0 +1,21 @@
+let score = "Ankush";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let scorevalueinNumaber = Number(score)
+console.log(typeof scorevalueinNumaber);
+
+console.log(scorevalueinNumaber);
+ 
+
+// NOTE:-
+// jab string value "33" hai to Number me convert ho jati.
+// jab "33abc" => NaN aati hai.
+// jab true => 1; or false => 0
+// or jab undefined type karte to value (NaN) Aati hai.
+
+
+let LoggedIn = "Ankush"
+
+let 
---
+++ basic-01/variables.js
@@ -0,0 +1,17 @@
+const accountid = 123456
+let Emails = "ankush@gmail.com"
+var Acoountpassword = "1234"
+accountcity = "ahmedabad"
+
+// const ko ek baar hi bata sakte hai baad me koi #const ki value ko nhi kr sakta 
+// accountid = 1305
+// console.log(accountid);
+Email = "aryan@gmail.com"
+// console.log(Emails);
+accountpassword = "13052000"
+accountcity = "Surat"
+// console.log(accountcity);
+
+console.table([Acoountpassword,Emails,accountcity])
+
+
