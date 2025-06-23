d494d61b477a1261bc2da7a25db9cff845eab447
ANKUSH <114465873+VILEN13@users.noreply.github.com>
2025-06-23 17:02:46 +0000

    #

--- basic-01/conversionoperation.js
+++ basic-01/conversionoperation.js
@@ -16,8 +16,35 @@
 // or jab undefined type karte to value (NaN) Aati hai.
 
 
-let LoggedIn =0
+// let LoggedIn =0
 
-let booleanisLoggedin = Boolean(LoggedIn)
-console.log(booleanisLoggedin);
+// let booleanisLoggedin = Boolean(LoggedIn)
+// console.log(booleanisLoggedin);
 
+
+// *************************************operation***************************
+
+
+
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+// console.log(value);
+
+// let str1 = "hello"
+// let str2 = " ankush"
+// let str3 = str1 + str2
+// console.log(str3);
+
+//****************TRICKy Conversion**************
+// console.log("1"+2);
+// console.log(2 + "1");
+// console.log("1"+ 2 + 3);
+// console.log(2 + 3+ "1");
+
+// console.log((3+4)*5 % 3);
+
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
