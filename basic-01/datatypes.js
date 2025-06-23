01f0aa0833b3d118979187b0f1d54530ba500340
ANKUSH <114465873+VILEN13@users.noreply.github.com>
2025-06-23 17:42:21 +0000

    #

--- basic-01/datatypes.js
+++ basic-01/datatypes.js
@@ -3,6 +3,18 @@
 //******************Primitive*************************
 //7 types : string, Number, Boolean, null, undefined, symbol, BigInt
 
+const score = 100
+const scorevalue = 100.5
+
+const inLoggedIn = false
+const outsidetemp = null
+let useremail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+// console.log(id===anotherid);
+
+const bignumber = 562154021541n
 
 
 
@@ -11,3 +23,15 @@
 //*********Refrence type (Nonprimitive)*******************
 
 // Array , Objects, Function
+
+// const heros= { "shaktiman", "nagraj", "doga"}
+// let myobj = {
+//     name: "hitesh"
+//     Age: 22,
+// }
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof myFunction);
