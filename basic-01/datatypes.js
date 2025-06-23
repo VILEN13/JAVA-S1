125ebfae147498c8fdc0c53ae459c0796c49ceb7
ANKUSH <114465873+VILEN13@users.noreply.github.com>
2025-06-23 17:08:49 +0000

    #

---
+++ basic-01/datatypes.js
@@ -0,0 +1,13 @@
+//data type is main 2 caterige hai primitive / nonprimitive
+
+//******************Primitive*************************
+//7 types : string, Number, Boolean, null, undefined, symbol, BigInt
+
+
+
+
+
+
+//*********Refrence type (Nonprimitive)*******************
+
+// Array , Objects, Function
