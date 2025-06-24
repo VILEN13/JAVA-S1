661e650b9206bc53ef25abdb5dd2586eab9c4d0c
ANKUSH <114465873+VILEN13@users.noreply.github.com>
2025-06-24 19:09:05 +0000

    #

--- basic-01/string.js
+++ basic-01/string.js
@@ -3,7 +3,35 @@ const repoCount = 50
 
 // console.log(name + repoCount + " value");
 
-console.log('Hello my name is ${name} and my repo count is ${repoCount}');
+// console.log('Hello my name is ${name} and my repo count is ${repoCount}');
 
 
+const gameName = new String('hitesh-hc-com')
+ //string prototypes (50-more) jese hote hai.
 
+//  console.log(gameName[3]);
+//  console.log(gameName.__proto__);
+
+//  console.log(gameName.length);
+//  console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(4));
+ console.log(gameName.indexOf('t'));
+
+const newstring = gameName.substring(0,4)
+console.log(newstring);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newstring01 = "     ANKUSH    "
+console.log(newstring01);
+console.log(newstring01.trim());
+
+const url = "htttps://hitesh.com/ankush%20choudhary"
+
+
+console.log(url.replace('%20',('_')));
+
+console.log(url.includes('hitesh'))
+
+console.log(gameName.split('-'));
