b43a077f3abd2df0bdb228d93d9452f8bd1ffca7
ANKUSH <114465873+VILEN13@users.noreply.github.com>
2025-06-24 17:58:34 +0000

    #

--- basic-01/datatypes.js
+++ basic-01/datatypes.js
@@ -30,8 +30,36 @@ const bignumber = 562154021541n
 //     Age: 22,
 // }
 
-const myFunction = function(){
-    console.log("hello world");
+// const myFunction = function(){
+//    console.log("hello world");
+// }
+
+//    console.log(typeof myFunction);
+
+
+//STACK (Primtive)     or    HEAP (non-primitive)
+
+let myYoutubename = "Ankushunknowchanel"
+
+let otherchanel = myYoutubename
+otherchanel = "chaiorcode"
+
+console.log(myYoutubename);
+console.log(otherchanel);
+
+//stack (primitive) me value ka copy diya jata original value me koi change nhi hota.
+
+
+let userone = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
 
-console.log(typeof myFunction);
+let usertwo = userone
+
+usertwo.email = "ankush@gmail.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
+
+//HEAP (non-primitive) me value ka refrence milta or main value me bhi change ho jata hai.
