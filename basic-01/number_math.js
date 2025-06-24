56dd7e82a608c59b654ec269042aa46caf758c26
ANKUSH <114465873+VILEN13@users.noreply.github.com>
2025-06-24 20:13:26 +0000

    #

--- basic-01/number_math.js
+++ basic-01/number_math.js
@@ -18,14 +18,26 @@
 
 //*********************************Math************************* */
 
-console.log(Math);  // math is a object.
-console.log(Math.abs(-4)); // .abs me negative value positive me convert hoti hai.
-console.log(Math.round(5.3)); //.round me round=off ho jata hai.
-console.log(Math.ceil(6.7)); //.ceil me decimal value (6.7) direct max 7 ho jati hai.
-console.log(Math.floor(6.7)); //.floor me decimal value (6.7) direct min 6 ho jati hai.
-console.log(Math.sqrt(25)); //.sqrt se squre value nikali jati hai.
+// console.log(Math);  // math is a object.
+// console.log(Math.abs(-4)); // .abs me negative value positive me convert hoti hai.
+// console.log(Math.round(5.3)); //.round me round=off ho jata hai.
+// console.log(Math.ceil(6.7)); //.ceil me decimal value (6.7) direct max 7 ho jati hai.
+// console.log(Math.floor(6.7)); //.floor me decimal value (6.7) direct min 6 ho jati hai.
+// console.log(Math.sqrt(25)); //.sqrt se squre value nikali jati hai.
+
+// console.log(Math.min(4,3,6,8)); //.min yeh array me min find karta hai.
+// console.log(Math.max(4,3,6,8)); //.max maximan value ok.
 
 
+
+// console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min);
 
 
 
