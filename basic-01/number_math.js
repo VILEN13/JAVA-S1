9f468363e03c76a9200436a0e1c6206a667a424a
ANKUSH <114465873+VILEN13@users.noreply.github.com>
2025-06-24 19:50:32 +0000

    #

---
+++ basic-01/number_math.js
@@ -0,0 +1,31 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length); //to string me number of length.
+// console.log(balance.toFixed(2)); //to fixed me number ke baad .00 lagta hai.
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4)); //decimal number ko round-off karne liye simle.
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); //long number ko(,) comma lagane k liye.
+
+
+
+
+//*********************************Math************************* */
+
+console.log(Math);  // math is a object.
+console.log(Math.abs(-4)); // .abs me negative value positive me convert hoti hai.
+console.log(Math.round(5.3)); //.round me round=off ho jata hai.
+console.log(Math.ceil(6.7)); //.ceil me decimal value (6.7) direct max 7 ho jati hai.
+console.log(Math.floor(6.7)); //.floor me decimal value (6.7) direct min 6 ho jati hai.
+console.log(Math.sqrt(25)); //.sqrt se squre value nikali jati hai.
+
+
+
+
+
